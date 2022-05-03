af3085e90c5d00a4a978e4cf127932be49b833ef
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-05-02 19:12:53 -0600

    Agregado el metodo getStudentCredits en StudentsServices y sus pruebas unitarias

--- test/service/StudentService.test.js
+++ test/service/StudentService.test.js
@@ -4,7 +4,6 @@ describe("Test de StudentService", () =>{
     test("Requerimiento 1: Recibir a todos los estudiantes", ()=>{
         const students = StudentServices.getAllStudents("./data/visualpartners.json");
         expect(typeof students).toBe("object");
-        //console.log(students);
     });
     test("Requerimiento 2: Recibir el Email de lso estudiantes con certificacion", ()=>{
         const students =[{
@@ -13,9 +12,13 @@ describe("Test de StudentService", () =>{
             "email": "Todd@visualpartnership.xyz",
             "haveCertification": true
         }];
-
-        console.log(StudentServices.getStudentEmails(students));
         const getEmail = StudentServices.getStudentEmails(students);
         expect(getEmail[0]).toEqual("Todd@visualpartnership.xyz");
+    });
+    test("Requerimiento 3: Obtener los estudiantes con creditos igual o mayores de 500", () =>{
+        const students =[{credits: 500},{credits: 600},{credits: 300}, {credits: 1000}, {credits: 100}];
+
+        const student1 = StudentServices.getStudentsCredits(students)[0];
+        expect(student1).toEqual({credits: 500});
     });
 });
